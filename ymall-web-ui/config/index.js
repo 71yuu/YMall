8550d8c6ba2185f84bed88ebe2f46f8b587ff366
Yuu <yuu71@qq.com>
2020-09-01 17:06:35 +0800

    增加部署文档

--- ymall-web-ui/config/index.js
+++ ymall-web-ui/config/index.js
@@ -1,4 +1,4 @@
-// see http://vuejs-templates.github.io/webpack for documentation.
+  // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
 
 module.exports = {
